aa05cb5d430212ca0d34c3881f3c755ffa6a62d8
Daniil Sviridov <daniil_sviridov@mail.ru>
2022-12-07 22:15:31 +0300

    fin

---
+++ LibrararyService.Web/Models/SearchType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibrararyService.Web.Models
+{
+    public enum SearchType
+    {
+
+        [Display(Name = "Автор")]
+        Autor,
+        Title,        
+        Category
+
+    }
+}
